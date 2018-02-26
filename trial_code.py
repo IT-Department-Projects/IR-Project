ecbab30b04e77335cf0c3255872fdde40fa3884c
Aiman Abdullah Anees <aimanabdullah65@gmail.com>
2018-02-26 19:06:08 +0530

    Add files via upload

--- trial_code.py
+++ trial_code.py
@@ -1,5 +1,4 @@
 import nltk
-<<<<<<< HEAD
 from nltk.corpus import stopwords,state_union,wordnet
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
@@ -7,9 +6,6 @@ from nltk.stem import WordNetLemmatizer
 lemmatizer=WordNetLemmatizer()
 ps=PorterStemmer()
 syns=wordnet.synsets("program")
-
-
-
 
 
 def performPreprocessing(query):
@@ -21,24 +17,15 @@ def performPreprocessing(query):
 	#named_entity=NER(token_list)
 	token_list=Lemmatizing(token_list)
 	return token_list
-=======
-from nltk.corpus import stopwords,state_union
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
-ps=PorterStemmer()
->>>>>>> 1a9d1f2a1f6674b8b3d09ac10da549439726d486
 
 
 
 ###Step1-Tokenize###
 def Tokenize(query):
 	token_list=word_tokenize(query)
-<<<<<<< HEAD
-=======
 	token_list=Stopword_removal(token_list)
 	token_list=Stemming(token_list)
-	token_list=POS_Removal(token_list)
->>>>>>> 1a9d1f2a1f6674b8b3d09ac10da549439726d486
+	token_list=POS_Tagging(token_list)
 	return token_list
 
 
@@ -54,18 +41,14 @@ def Stemming(token_list):
 		token_list[i]=ps.stem(token_list[i])
 	return token_list
 
-<<<<<<< HEAD
+
 ###Step4-Parts Of Speech Tagging###
 def POS_Tagging(token_list):
-=======
-###Step4-Parts Of Speech Removal###
-def POS_Removal(token_list):
->>>>>>> 1a9d1f2a1f6674b8b3d09ac10da549439726d486
 	token_list=nltk.pos_tag(token_list)
 	return token_list
 
 
-<<<<<<< HEAD
+
 ###Step5-Chunking&Chinking###
 def Chunking(token_list):
 	chunkGram=r"""Chunk: {<.*>+}
@@ -92,11 +75,7 @@ query=state_union.raw('2005-GWBush.txt')
 #print(result)
 
 print(syns)
-=======
-
-
-
 query=state_union.raw('2005-GWBush.txt')
 result=Tokenize(query)
 print(result)
->>>>>>> 1a9d1f2a1f6674b8b3d09ac10da549439726d486
+
