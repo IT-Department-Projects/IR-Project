863b1b3eb1716c173c7143c925a770667de0365c
Aiman Abdullah Anees <aimanabdullah65@gmail.com>
2018-03-29 15:54:19 +0400

    VSM Model

---
+++ VSM.py
@@ -0,0 +1,114 @@
+import ast
+import string
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from collections import Counter
+import simplejson
+import csv
+
+
+###Accessing the Corpus###
+file_path='key_fields.json'
+f1 = open(file_path, 'r')
+corpus=f1.read()
+corpus = ast.literal_eval(corpus)
+f1.close()
+
+
+###Normalization###
+for i in range(len(corpus)):
+	corpus[i]['Title']=corpus[i]['Title'].lower()
+	corpus[i]['Description']=corpus[i]['Description'].lower()
+
+
+
+###Tokenization###
+for i in range(len(corpus)):
+	corpus[i]['Title']=word_tokenize(corpus[i]['Title'])
+	corpus[i]['Description']=word_tokenize(corpus[i]['Description'])
+
+
+###Stemming###
+ps=PorterStemmer()
+for i in range(len(corpus)):
+	for j in range(len(corpus[i]['Title'])):
+		corpus[i]['Title'][j]=ps.stem(corpus[i]['Title'][j])
+	for k in range(len(corpus[i]['Description'])):
+		corpus[i]['Description'][k]=ps.stem(corpus[i]['Description'][k])
+
+
+
+###Lemmatization###
+lemmatizer=WordNetLemmatizer()
+for i in range(len(corpus)):
+	for j in range(len(corpus[i]['Title'])):
+		corpus[i]['Title'][j]=lemmatizer.lemmatize(corpus[i]['Title'][j])
+	for k in range(len(corpus[i]['Description'])):
+		corpus[i]['Description'][k]=lemmatizer.lemmatize(corpus[i]['Description'][k])
+
+
+
+"""
+Cleaning should have the following steps:
+(i) Removing tokens that contains punctuations
+(ii) Removing tokens that are just punctuations
+(iii) Removing tokens that have one character 
+"""
+
+###(i) & (ii)###
+translation = str.maketrans("","", string.punctuation)
+for i in range(len(corpus)):
+	for j in range(len(corpus[i]['Title'])):
+		corpus[i]['Title'][j]=corpus[i]['Title'][j].translate(translation)
+	for k in range(len(corpus[i]['Description'])):
+		corpus[i]['Description'][k]=corpus[i]['Description'][k].translate(translation)
+
+
+###(iii)###
+for i in range(len(corpus)):
+	corpus[i]['Title']=[token for token in corpus[i]['Title'] if len(token)>2]
+	corpus[i]['Description']=[token for token in corpus[i]['Description'] if len(token)>2]
+
+
+
+
+###Stopword Removal###
+stop_words = set(stopwords.words('english'))
+for i in range(len(corpus)):
+	corpus[i]['Title']=[token for token in corpus[i]['Title'] if not token in stop_words]
+	corpus[i]['Description']=[token for token in corpus[i]['Description'] if not token in stop_words]
+
+
+###Creating Vocabulary###
+vocabulary = Counter()
+for i in range(len(corpus)):
+	vocabulary.update(corpus[i]['Title'])
+	vocabulary.update(corpus[i]['Description'])
+
+vocabulary_list = [word for word,frequency in vocabulary.items() if frequency >= 5 and len(word)>1]
+file_path='vocabulary.txt'
+f = open(file_path, 'w')
+simplejson.dump(vocabulary_list, f)
+f.close()
+
+
+###Saving the Preprocessed Documents###
+documents=[]
+for i in range(len((corpus))):
+	documents.append(corpus[i]['Title'])
+for i in range(len(corpus)):
+	documents.append(corpus[i]['Description'])
+
+file_path='documents.txt'
+f = open(file_path, 'w')
+simplejson.dump(documents, f)
+f.close()
+
+print("Preprocessing Successful!")
+
+
+
+
+
